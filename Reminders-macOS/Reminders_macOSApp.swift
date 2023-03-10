042f98b330c6320365d51ae4d9be6158d27d91dc
mcantop <maciejcantop@icloud.com>
2023-03-10 01:42:59 +0100

    Initial Commit

---
+++ Reminders-macOS/Reminders_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_macOSApp.swift
+//  Reminders-macOS
+//
+//  Created by Maciej on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
